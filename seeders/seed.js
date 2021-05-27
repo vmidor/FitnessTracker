f1290f9ad0c1dd588848919f76517ded0c523612
vmidor <76964172+vmidor@users.noreply.github.com>
2021-05-26 21:40:31 -0400

    changed models

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 let mongoose = require("mongoose");
-let db = require("../models/index");
+let db = require("../models");
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/workoutdb',
